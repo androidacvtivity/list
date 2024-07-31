48e430e71346c22e99d4a81a57ef0acda2170e73
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2024-07-31 12:51:07 +0300

    DetailActivityvw_ro

--- app/src/main/java/com/bancusoft/list/Views/DetailActivityvw_ro.java
+++ app/src/main/java/com/bancusoft/list/Views/DetailActivityvw_ro.java
@@ -4,10 +4,13 @@ import static com.bancusoft.list.Helpers.Utils.app_google;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -48,20 +51,72 @@ public class DetailActivityvw_ro extends AppCompatActivity{
         receivedScientist_ro = Utils.receiveScientistvw_ro(getIntent(), DetailActivityvw_ro.this);
 
         if (receivedScientist_ro != null) {
-            Nume_firma.setText(receivedScientist_ro.getNume_firma().trim());
-            cui.setText(receivedScientist_ro.getCui().trim());
-            cod_inmatriculare.setText(receivedScientist_ro.getcod_inmatriculare().trim());
-            EUID.setText(receivedScientist_ro.getEUID().trim());
-            stare.setText(receivedScientist_ro.getStare().trim());
-            Sediu.setText(receivedScientist_ro.getSediu().trim());
-            country.setText(receivedScientist_ro.getCountry().trim());
-            Localitate.setText(receivedScientist_ro.getLocalitate().trim());
-            Judet.setText(receivedScientist_ro.getJudet().trim());
-            act.setText(receivedScientist_ro.getAct().trim());
+
+//            //Same
+//            Nume_firma.setText(receivedScientist_ro.getNume_firma().trim());
+//            cui.setText(receivedScientist_ro.getCui().trim());
+//            cod_inmatriculare.setText(receivedScientist_ro.getcod_inmatriculare().trim());
+//            EUID.setText(receivedScientist_ro.getEUID().trim());
+//            stare.setText(receivedScientist_ro.getStare().trim());
+//            Sediu.setText(receivedScientist_ro.getSediu().trim());
+//            country.setText(receivedScientist_ro.getCountry().trim());
+//            Localitate.setText(receivedScientist_ro.getLocalitate().trim());
+//            Judet.setText(receivedScientist_ro.getJudet().trim());
+//            act.setText(receivedScientist_ro.getAct().trim());
+//
+//
+//            mCollapsingToolbarLayoutvw_ro.setTitle(receivedScientist_ro.getCui());
+//            mCollapsingToolbarLayoutvw_ro.setExpandedTitleColor(ContextCompat.getColor(this, R.color.white));
+
+            // Check if the TextViews and receivedScientist_ro are not null before setting text
+            if (Nume_firma != null && receivedScientist_ro.getNume_firma() != null) {
+                Nume_firma.setText(receivedScientist_ro.getNume_firma().trim());
+            }
+
+            if (cui != null && receivedScientist_ro.getCui() != null) {
+                cui.setText(receivedScientist_ro.getCui().trim());
+            }
+
+            if (cod_inmatriculare != null && receivedScientist_ro.getcod_inmatriculare() != null) {
+                cod_inmatriculare.setText(receivedScientist_ro.getcod_inmatriculare().trim());
+            }
+
+            if (EUID != null && receivedScientist_ro.getEUID() != null) {
+                EUID.setText(receivedScientist_ro.getEUID().trim());
+            }
+
+            if (stare != null && receivedScientist_ro.getStare() != null) {
+                stare.setText(receivedScientist_ro.getStare().trim());
+            }
+
+            if (Sediu != null && receivedScientist_ro.getSediu() != null) {
+                Sediu.setText(receivedScientist_ro.getSediu().trim());
+            }
+
+            if (country != null && receivedScientist_ro.getCountry() != null) {
+                country.setText(receivedScientist_ro.getCountry().trim());
+            }
+
+            if (Localitate != null && receivedScientist_ro.getLocalitate() != null) {
+                Localitate.setText(receivedScientist_ro.getLocalitate().trim());
+            }
+
+            if (Judet != null && receivedScientist_ro.getJudet() != null) {
+                Judet.setText(receivedScientist_ro.getJudet().trim());
+            }
+
+            if (act != null && receivedScientist_ro.getAct() != null) {
+                act.setText(receivedScientist_ro.getAct().trim());
+            }
+
+// Set title for CollapsingToolbarLayout
+            if (mCollapsingToolbarLayoutvw_ro != null && receivedScientist_ro.getCui() != null) {
+                mCollapsingToolbarLayoutvw_ro.setTitle(receivedScientist_ro.getCui());
+                mCollapsingToolbarLayoutvw_ro.setExpandedTitleColor(ContextCompat.getColor(this, R.color.white));
+            }
 
 
-            mCollapsingToolbarLayoutvw_ro.setTitle(receivedScientist_ro.getCui());
-            mCollapsingToolbarLayoutvw_ro.setExpandedTitleColor(ContextCompat.getColor(this, R.color.white));
+
         }
     }
 
@@ -74,6 +129,72 @@ public class DetailActivityvw_ro extends AppCompatActivity{
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
     }
+
+
+
+
+    /**
+     * When a menu item is selected we want to navigate to the appropriate page
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+//        if (id==R.id.action_edit_vw){
+//            Utils.sendScientistvwToActivity(this,receivedScientist,help_vw.class);
+//            finish();
+//            return true;
+//        }
+//        else
+//
+//        if (id==R.id.action_edit_en_vw){
+//            Utils.sendScientistvwToActivity(this,receivedScientist,help_vw_en.class);
+//            finish();
+//            return true;
+//
+//        }
+//
+//        else
+//        if (id==R.id.action_edit_ru_vw){
+//            Utils.sendScientistvwToActivity(this,receivedScientist,help_vw_ru.class);
+//            finish();
+//            return true;
+//        }
+//
+//
+//        else
+
+        if (id == android.R.id.home){
+
+            Intent intent;
+            intent = new Intent(this, ScientistsActivityvw_ro.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            finish();
+            startActivity(intent);
+            return true;
+
+        }
+
+        else
+        if (id == R.id.video3_vw){
+
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(com.bancusoft.list.Helpers.Utils.youtube_level_stat));
+
+            startActivity(browserIntent);
+
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -94,16 +215,30 @@ public class DetailActivityvw_ro extends AppCompatActivity{
         Button mBtnShare = findViewById(R.id.share_ro_det);
 
         mBtnShare.setOnClickListener(view -> {
-            String s_Nume_firma = Nume_firma.getText().toString();
-            String s_cui = cui.getText().toString();
-            String s_cod_inmatriculare = cod_inmatriculare.getText().toString();
-            String  s_EUID = EUID.getText().toString();
-            String  s_stare = stare.getText().toString();
-            String  s_Sediu = Sediu.getText().toString();
-            String  s_country = country.getText().toString();
-            String  s_Localitate = Localitate.getText().toString();
-            String  s_Judet = Judet.getText().toString();
-            String  s_act = act.getText().toString();
+
+//            // Ensure TextViews are not null before retrieving their text
+//            String s_Nume_firma = Nume_firma.getText().toString();
+//            String s_cui = cui.getText().toString();
+//            String s_cod_inmatriculare = cod_inmatriculare.getText().toString();
+//            String  s_EUID = EUID.getText().toString();
+//            String  s_stare = stare.getText().toString();
+//            String  s_Sediu = Sediu.getText().toString();
+//            String  s_country = country.getText().toString();
+//            String  s_Localitate = Localitate.getText().toString();
+//            String  s_Judet = Judet.getText().toString();
+//            String  s_act = act.getText().toString();
+
+            // Ensure TextViews are not null before retrieving their text
+            String s_Nume_firma = Nume_firma != null ? Nume_firma.getText().toString() : "";
+            String s_cui = cui != null ? cui.getText().toString() : "";
+            String s_cod_inmatriculare = cod_inmatriculare != null ? cod_inmatriculare.getText().toString() : "";
+            String s_EUID = EUID != null ? EUID.getText().toString() : "";
+            String s_stare = stare != null ? stare.getText().toString() : "";
+            String s_Sediu = Sediu != null ? Sediu.getText().toString() : "";
+            String s_country = country != null ? country.getText().toString() : "";
+            String s_Localitate = Localitate != null ? Localitate.getText().toString() : "";
+            String s_Judet = Judet != null ? Judet.getText().toString() : "";
+            String s_act = act != null ? act.getText().toString() : "";
 
 
             String contentShare = " Nume firmă : " + s_Nume_firma + " - Cui :  " + s_cui + " - Cod Inmatriculare - " + s_cod_inmatriculare
@@ -112,7 +247,7 @@ public class DetailActivityvw_ro extends AppCompatActivity{
                     + " -  Țara :  " + s_country
                     + " -  : Localitate " + s_Localitate
                     + " -  : Județ :  " +  s_Judet
-                    + " -  actualizare" + s_act + " -- The application -Level Stat - can be downloaded from here "
+                    + " -  actualizare" + s_act + " -- The application -Stat Level  - can be downloaded from here "
                     +  app_google;
 
             Intent sharingIntent = new Intent(Intent.ACTION_SEND);
@@ -124,7 +259,24 @@ public class DetailActivityvw_ro extends AppCompatActivity{
             startActivity(Intent.createChooser(sharingIntent, "Share text via"));
         });
 
+ //-----------------------------
 
+        // Register the onBackPressed callback
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                // Handle the back button event
+                Intent intent = new Intent(DetailActivityvw_ro.this, ScientistsActivityvw_ro.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                finish();
+                startActivity(intent);
+            }
+        };
+
+        // Add the callback to the dispatcher
+        getOnBackPressedDispatcher().addCallback(this, callback);
+
+//-----------------------------
 
         initializeWidgets();
         receiveAndShowData();
