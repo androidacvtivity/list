3fcdc5848450a5fbce7b2cb8ae874597f72d73be
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2024-10-15 11:14:46 +0300

    DetailActivityclcocm

--- app/src/main/java/com/bancusoft/list/Views/DetailActivityclcocm.java
+++ app/src/main/java/com/bancusoft/list/Views/DetailActivityclcocm.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -146,18 +147,6 @@ public class DetailActivityclcocm extends AppCompatActivity {
         super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
     }
 
-    /**
-     * Let's finish the current activity when back button is pressed
-     */
-
-    @Override
-    public void onBackPressed() {
-        Intent intent;
-        intent = new Intent(this, ScientistsActivityclcocm.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        finish();
-        startActivity(intent);
-    }
 
 
 
@@ -192,7 +181,19 @@ public class DetailActivityclcocm extends AppCompatActivity {
             startActivity(Intent.createChooser(sharingIntent, "Share text via"));
         });
 
+//-------------
 
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                Intent intent = new Intent(DetailActivityclcocm.this, ScientistsActivityclcocm.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+//----------------
 
         initializeWidgets();
         receiveAndShowData();
