18ce2c8ee876d973da4384c08c7852699991d374
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2024-10-16 08:19:59 +0300

    versionCode 20

--- app/src/main/java/com/bancusoft/list/Views/DetailActivityclcaem.java
+++ app/src/main/java/com/bancusoft/list/Views/DetailActivityclcaem.java
@@ -26,51 +26,33 @@ import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
 public class DetailActivityclcaem extends AppCompatActivity {
 
-    //Let's define our instance fields
-    private TextView CODUL_TV_CAEM ,DENUMIRE_TV_caem;
-    private final Cl_caem receivedCl_caem;
+    private TextView CODUL_TV_CAEM, DENUMIRE_TV_caem;
+    private Cl_caem receivedCl_caem;  // Removed 'final' keyword
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
 
-    public DetailActivityclcaem(Cl_caem receivedClCaem) {
-        receivedCl_caem = receivedClCaem;
-    }
+    // Removed the parameterized constructor
 
     /**
-     * Let's initialize our widgets
+     * Initialize the widgets
      */
     private void initializeWidgets(){
         CODUL_TV_CAEM = findViewById(R.id.CODUL_TV_CAEM);
         DENUMIRE_TV_caem = findViewById(R.id.DENUMIRE_TV_caem);
-        mCollapsingToolbarLayout=findViewById(R.id.mCollapsingToolbarLayoutclcaem);
+        mCollapsingToolbarLayout = findViewById(R.id.mCollapsingToolbarLayoutclcaem);
     }
-
-
-//I modify you code I use
-// public class ViewUtils {
-//    public static void setTextSafely(TextView textView, String text) {
-//        if (textView != null && text != null) {
-//            textView.setText(text.trim());
-//        }
-//    }
-//}
-    //What do you think ?
 
     private void receiveAndShowData() {
         // Retrieve the Cl_caem object from the Intent
-        Cl_caem receivedCl_caem = Utils.receiveCl_caem(getIntent(), DetailActivityclcaem.this);
+        receivedCl_caem = Utils.receiveCl_caem(getIntent(), DetailActivityclcaem.this);
 
-        // Check if the received object is not null
         if (receivedCl_caem != null) {
-            // Update the UI components with data from the received Cl_caem object, or set to empty string if null
-
+            // Update UI components with data from Cl_caem object
             ViewUtils.setTextSafely(CODUL_TV_CAEM, receivedCl_caem.getCODUL());
             ViewUtils.setTextSafely(DENUMIRE_TV_caem, receivedCl_caem.getDENUMIRE());
             mCollapsingToolbarLayout.setTitle(receivedCl_caem.getCODUL() != null ? receivedCl_caem.getCODUL() : "");
             mCollapsingToolbarLayout.setExpandedTitleColor(ContextCompat.getColor(this, R.color.white));
         } else {
-            // Handle the case where receivedCl_caem is null (optional)
             Log.e("DetailActivity", "Received Cl_caem is null");
-            // Setting empty strings to TextViews if receivedCl_caem is null
             CODUL_TV_CAEM.setText("");
             DENUMIRE_TV_caem.setText("");
             mCollapsingToolbarLayout.setTitle("");
@@ -78,9 +60,8 @@ public class DetailActivityclcaem extends AppCompatActivity {
         }
     }
 
-
     /**
-     * Let's inflate our menu for the detail page
+     * Inflate menu for detail page
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -88,64 +69,45 @@ public class DetailActivityclcaem extends AppCompatActivity {
         return true;
     }
 
-
     /**
-     * When a menu item is selected we want to navigate to the appropriate page
+     * Handle menu item selections
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
-    int id = item.getItemId();
-
-        if (id==R.id.action_edit_caem){
-            Utils.sendCl_caemToActivity(this,receivedCl_caem,help_vw.class);
-                finish();
-                return true;
-        }
-        else
-
-        if (id==R.id.action_edit_en_caem){
-            Utils.sendCl_caemToActivity(this,receivedCl_caem,help_vw_en.class);
-                finish();
-                return true;
-        }
-        else
-        if (id==R.id.action_edit_ru_caem){
-            Utils.sendCl_caemToActivity(this,receivedCl_caem,help_vw_ru.class);
-                finish();
-                return true;
-        }
-        else
-
-        if (id == android.R.id.home){
-
-            Intent intent;
-            intent = new Intent(this,ScientistsActivityclcaem.class);
+        if (id == R.id.action_edit_caem) {
+            Utils.sendCl_caemToActivity(this, receivedCl_caem, help_vw.class);
+            finish();
+            return true;
+        } else if (id == R.id.action_edit_en_caem) {
+            Utils.sendCl_caemToActivity(this, receivedCl_caem, help_vw_en.class);
+            finish();
+            return true;
+        } else if (id == R.id.action_edit_ru_caem) {
+            Utils.sendCl_caemToActivity(this, receivedCl_caem, help_vw_ru.class);
+            finish();
+            return true;
+        } else if (id == android.R.id.home) {
+            Intent intent = new Intent(this, ScientistsActivityclcaem.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
             finish();
             startActivity(intent);
             return true;
-
-        }
-        else
-        if (id == R.id.video3_caem){
-
+        } else if (id == R.id.video3_caem) {
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(com.bancusoft.list.Helpers.Utils.youtube_level_stat));
             startActivity(browserIntent);
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
-    /**
-     * Let's once again override the attachBaseContext. We do this for our
-     * Calligraphy library
-     */
+
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
     }
 
-//Not, only in this case
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -161,40 +123,26 @@ public class DetailActivityclcaem extends AppCompatActivity {
             }
         });
 
-        CODUL_TV_CAEM = findViewById(R.id.CODUL_TV_CAEM);
-        DENUMIRE_TV_caem = findViewById(R.id.DENUMIRE_TV_caem);
-
+        initializeWidgets();
 
         Button mBtnShare = findViewById(R.id.btnShare_caem);
-
         mBtnShare.setOnClickListener(view -> {
-
-            //Modify in this case same
             String s_CODUL_TV_CAEM = CODUL_TV_CAEM.getText().toString();
             String s_DENUMIRE_TV_caem = DENUMIRE_TV_caem.getText().toString();
-
-
             String contentShare = " Codul CAEM : " + s_CODUL_TV_CAEM + " - Denumirea activitatii, \n" +
-                    "  :  " + s_DENUMIRE_TV_caem
-
-                    + " -- The application -Level Stat - can be downloaded from here "
-                    +  app_google;
+                    "  :  " + s_DENUMIRE_TV_caem +
+                    " -- The application -Level Stat - can be downloaded from here " +
+                    app_google;
 
             Intent sharingIntent = new Intent(Intent.ACTION_SEND);
             sharingIntent.setType("text/plain");
-
             sharingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "CLASIFICATORUL ACTIVITĂŢILOR \n" +
-                    "DIN ECONOMIA MOLDOVEI \n" +
-                    "(CAEM)\n");
+            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "CLASIFICATORUL ACTIVITĂŢILOR \nDIN ECONOMIA MOLDOVEI \n(CAEM)\n");
             sharingIntent.putExtra(Intent.EXTRA_TEXT, contentShare);
 
             startActivity(Intent.createChooser(sharingIntent, "Share text via"));
         });
 
-
-
-        initializeWidgets();
         receiveAndShowData();
     }
 }
